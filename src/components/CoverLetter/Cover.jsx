cccca1d68b54aae8767244ba8af04b90f59ee30f
ZadAndRot <kasirecivanna@gmail.com>
2022-09-04 22:00:46 +0300

    feat: :sparkles:

--- src/components/CoverLetter/Cover.jsx
+++ src/components/CoverLetter/Cover.jsx
@@ -2,20 +2,35 @@ import '../CoverLetter/Cover.scss'
 import { nanoid } from 'nanoid'
 
 const Cover = (props) => {
-  const { country, textarea, copyText } = props
+  const { country, textarea, data, firstFlow, secondFlow ,name} = props
   return (
-    <div className="cover" ref={copyText}>
+    <div className="cover" >
       <div className="hey">Hey!{country}</div>
-      <div className="intro">
-        
-      </div>
+      <br /> <br />
       {textarea.map((el) => (
         <div key={nanoid()} className="textarea">
           {el}
+          <br /> <br />
         </div>
       ))}
+      <p>{firstFlow}</p>
+      <br />
+      <br />
+      {data.map((el) => (
+        <div key={nanoid()}>
+          <p>{el.url}</p> <br />
+          <p>{el.text}</p>
+          <br />
+          <br />
+        </div>
+      ))}
+      <p>{secondFlow}</p><br/>
+      <p>See soon,</p>
+      <p>{name}</p>
     </div>
   )
 }
 
 export default Cover
+
+
